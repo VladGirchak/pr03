f86dca8cbcd470e96fe21c42619679683a07c31a
VladGirchak <119739586+VladGirchak@users.noreply.github.com>
2024-04-18 22:58:26 +0300

    Add files via upload

---
+++ pr03.py
@@ -0,0 +1,85 @@
+# Музичний інструмент
+class MusicalInstrument:
+    def name(self):
+        pass
+
+
+###
+
+
+# Бубон
+class Trumpet(MusicalInstrument):
+    def name(self):
+        return "Бубон"
+
+
+# Барабан
+class Drum(MusicalInstrument):
+    def name(self):
+        return "Барабан"
+
+
+# Тромбон
+class Tambourine(MusicalInstrument):
+    def name(self):
+        return "Тромбон"
+
+
+# Флейта
+class Flute(MusicalInstrument):
+    def name(self):
+        return "Флейта"
+
+
+# Орган
+class Organ(MusicalInstrument):
+    def name(self):
+        return "Орган"
+
+
+# Гармоніка
+class Harmonica(MusicalInstrument):
+    def name(self):
+        return "Гармоніка"
+
+
+# Баян
+class Banjo(MusicalInstrument):
+    def name(self):
+        return "Баян"
+
+
+# Рояль
+class Harp(MusicalInstrument):
+    def name(self):
+        return "Рояль"
+
+
+###
+
+
+# Ударні
+class Strike(Trumpet, Drum):
+    pass
+
+
+# Духові
+class Percussion(Tambourine, Flute, Organ):
+    pass
+
+
+# Клавішні
+class Keyboard(Harmonica, Banjo, Harp):
+    pass
+
+
+###
+
+# Виведе "Бубон", тому що перший клас який наслідує Strike є Trumpet
+print(Strike().name())
+
+# Виведе "Тромбон", тому що перший клас який наслідує Percussion є Tambourine
+print(Percussion().name())
+
+# Виведе "Гармоніка", тому що перший клас який наслідує Keyboard є Harmonica
+print(Keyboard().name())
